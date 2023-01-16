eb7373bf47b0ee75501324afbc76a768bc8f6311
MSzoot <97002088+MSzoot@users.noreply.github.com>
2023-01-16 14:11:24 +0000

    up

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const lettersWithSpecials = ["A","B","C","D","E","F","G","H","I","J","K","L","M"
 
 
 
-// id to variables
+// set variables from ID
 
 const specials = document.getElementById("specials");
 const numbers = document.getElementById("numbers");
